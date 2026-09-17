093a878612552e049016ec78a985637887f3b991
nahar288 <mmhossainmollah@gmail.com>
2026-09-17 14:59:04 +0600

    completed the project

--- main.py
+++ main.py
@@ -39,3 +39,35 @@ initial_balance = float(input("Enter initial balance: "))
 
 account = BankAccount(account_number, account_holder, initial_balance)
 
+while True:
+
+    print("===== BANK MENU =====")
+    print("1. Check Balance")
+    print("2. Deposit")
+    print("3. Withdraw")
+    print("4. Set Balance")
+    print("5. Exit")
+
+    choice = input("Choose an option: ")
+
+    if choice == "1":
+        account.check_balance()
+
+    elif choice == "2":
+        amount = float(input("Enter deposit amount: "))
+        account.deposit(amount)
+
+    elif choice == "3":
+        amount = float(input("Enter withdrawal amount: "))
+        account.withdraw(amount)
+
+    elif choice == "4":
+        amount = float(input("Enter new balance: "))
+        account.set_balance(amount)
+
+    elif choice == "5":
+        print("dhonnobash🤑")
+        break
+
+    else:
+        print("Invalid option. Please choose 1-5.")
